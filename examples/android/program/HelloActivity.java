d1aa111d4d151507f5cc584cf15584cf1489785b
Menny Even Danan <menny@evendanan.net>
2018-12-31 20:44:12 -0500

    Adding Android example

---
+++ examples/android/program/HelloActivity.java
@@ -0,0 +1,17 @@
+package net.evendanan.bazel.mvn.example;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+public class HelloActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable final Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.main_activity);
+    }
+}
