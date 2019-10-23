60568ab58c62afc6c800b00d137618e132d72e70
Magnus Sköld <mask@spotify.com>
2019-10-23 16:28:42 -0400

    Add debug logs to RuleToDependency

--- resolver/src/main/java/com/google/devtools/bazel/workspace/maven/adapter/RuleToDependency.java
+++ resolver/src/main/java/com/google/devtools/bazel/workspace/maven/adapter/RuleToDependency.java
@@ -10,12 +10,26 @@ import net.evendanan.bazel.mvn.api.License;
 
 public class RuleToDependency {
 
-    public static Dependency from(Rule rule) {
+    private final boolean debugLogs;
+
+    public RuleToDependency(boolean debugLogs) {
+        this.debugLogs = debugLogs;
+    }
+
+    public Dependency from(Rule rule) {
         return from(rule, new HashMap<>());
     }
 
-    public static Dependency from(Rule rule, HashMap<String, Dependency> cache) {
-        if (cache.containsKey(rule.mavenCoordinates())) return cache.get(rule.mavenCoordinates());
+    public Dependency from(Rule rule, HashMap<String, Dependency> cache) {
+        if (debugLogs) {
+            System.out.println("Resolving dependencies for " + rule.mavenCoordinates());
+        }
+        if (cache.containsKey(rule.mavenCoordinates())) {
+            if (debugLogs) {
+                System.out.println("Dependencies taken from cache for " + rule.mavenCoordinates());
+            }
+            return cache.get(rule.mavenCoordinates());
+        }
 
         Dependency mapped = new Dependency(rule.groupId(), rule.artifactId(), rule.version(), rule.packaging(),
                 rule.getDeps().stream().map(dep -> from(dep, cache)).collect(Collectors.toList()),
@@ -30,6 +44,9 @@ public class RuleToDependency {
                         .filter(Objects::nonNull)
                         .collect(Collectors.toList()));
 
+        if (debugLogs) {
+            System.out.println("Caching dependencies for " + rule.mavenCoordinates());
+        }
         cache.put(rule.mavenCoordinates(), mapped);
         return mapped;
     }
