bc50c07ee8b9f5a40d55e6ae7a0b2480831a7e55
Menny Even Danan <menny@evendanan.net>
2021-05-26 15:02:28 -0400

    Add README details for type argument

--- resolver/src/main/java/net/evendanan/bazel/mvn/api/model/TargetType.java
+++ resolver/src/main/java/net/evendanan/bazel/mvn/api/model/TargetType.java
@@ -1,11 +1,38 @@
 package net.evendanan.bazel.mvn.api.model;
 
 public enum TargetType {
+    /**
+     * Plain java target.
+     */
     jar,
+
+    /**
+     * Plain Android target (Java).
+     */
     aar,
+
+    /**
+     * Pure Kotlin library.
+     */
     kotlin,
+
+    /**
+     * Kotlin library with Android support.
+     */
     kotlin_aar,
+
+    /**
+     * Very fast detector for pom, jar, aar, kotlin and kotlin-aar. Does not detect annotation-processors.
+     */
     naive,
+
+    /**
+     * Java annotation-processor target (java-plugin).
+     */
     processor,
+
+    /**
+     * Slow detector. Will inspect the artifact and determine what it is.
+     */
     auto
 }
